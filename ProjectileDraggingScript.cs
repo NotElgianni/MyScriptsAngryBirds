903f3e6272a8386ff32d622bb49967c90f31b4f3
Elgianni <22642@ma-web.nl>
2018-02-02 19:04:34 +0100

    Added my own scripts

---
+++ ProjectileDraggingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDraggingScript : MonoBehaviour
+{
+    private bool Clicked;
+    public bool BeenShot;
+    private bool IsSelected;
+    public bool freezeRotation;
+
+    private void Start()
+    {
+        Clicked = false;
+        BeenShot = false;
+        IsSelected = false;
+    }
+
+    void Update()
+    {
+        if (!IsSelected)
+        {
+            if (!BeenShot)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    transform.position = (pos);
+                    Clicked = true;
+                }
+                else if (Clicked)
+                {
+                    GetComponent<SpringJoint2D>().enabled = false;
+                    Clicked = false;
+                    BeenShot = true;
+                }
+            }
+            else
+            {
+                //UnFreeze rigidbody rotation
+
+            }
+        }
+    }
+}
---
+++ TileScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+
+
+    public float TileHealth = 4f;
+
+
+    // Use this for initialization
+    void Start()
+        {
+            
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.relativeVelocity.magnitude < 8 && collision.relativeVelocity.magnitude > 3)
+            {
+                TileHealth -= 1f;
+            }
+
+            else if (collision.relativeVelocity.magnitude < 15 && collision.relativeVelocity.magnitude > 8)
+            {
+                TileHealth -= 2f;
+            }
+
+            else if (collision.relativeVelocity.magnitude > 15)
+            {
+                TileHealth -= 3f;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Debug.Log(TileHealth);
+            if (TileHealth < 1f)
+            {
+                Destroy(gameObject);
+            }
+        }
+}
